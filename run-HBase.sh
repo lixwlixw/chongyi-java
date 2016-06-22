0b7e2fa9fbadca3bfaa478aa3b4b843358283aba
MySQL <871810908@qq.com>
2016-06-22 14:39:02 +0800

    Update run-HBase.sh

--- run-HBase.sh
+++ run-HBase.sh
@@ -10,6 +10,15 @@ do
  export LIB=$LIB:$jarfile
 done
 
+for jarfile in `ls /usr/lib/hadoop/hadoop-*.jar`
+do
+ export LIB=$LIB:$jarfile
+done
+
+for jarfile in `ls /usr/lib/hadoop/lib/*.jar`
+do
+ export LIB=$LIB:$jarfile
+done
 #echo CLASSPATH=$CLASSPATH
 
 java -cp  $LIB:AV-HBase-thrift-S.jar com.main.AV_HBase_Thrift_S >>1.log 2>&1 &
