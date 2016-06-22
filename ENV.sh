f070a7d679d93d1cc60cc06619b9dd773717c04a
MySQL <871810908@qq.com>
2016-06-22 19:51:44 +0800

    Update ENV.sh

--- ENV.sh
+++ ENV.sh
@@ -15,7 +15,7 @@ set -e
     sed -i 's/10.191.116.75:9092,10.191.116.76:9092/'$kahost'/g' /lib/skyEye/AV-SDK-Thrift/conf/AV.cfg
     sed -i 's/10.1.0.21/127.0.0.1/g' /lib/skyEye/AV-Master/conf/sky-site.xml
     sed -i 's/10.1.0.21/127.0.0.1/g' /lib/skyEye/AV-HBase-thrift-S/conf/sky-site.xml
-    bash /lib/skyEye/AV-SDK-Thrift/run-SDK.sh
-    bash /lib/skyEye/AV-Master/run-MASTER.sh
-    bash /lib/skyEye/AV-HBase-thrift-S/run-HBase.sh
+bash /lib/skyEye/AV-SDK-Thrift/run-SDK.sh
+bash /lib/skyEye/AV-Master/run-MASTER.sh
+bash /lib/skyEye/AV-HBase-thrift-S/run-HBase.sh
     
