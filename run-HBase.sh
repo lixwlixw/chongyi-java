c43b6d0df63c44591da1f27fe62795eafef48924
MySQL <871810908@qq.com>
2016-06-22 13:20:17 +0800

    Create run-HBase.sh

---
+++ run-HBase.sh
@@ -0,0 +1,15 @@
+LIB=$PATH
+for jarfile in `ls /lib/skyEye/AV-lib/*.jar`
+
+do
+ export LIB=$LIB:$jarfile
+done
+
+for jarfile in `ls /usr/lib/hbase/lib/*.jar`
+do
+ export LIB=$LIB:$jarfile
+done
+
+#echo CLASSPATH=$CLASSPATH
+
+java -cp  $LIB:AV-HBase-thrift-S.jar com.main.AV_HBase_Thrift_S >>1.log 2>&1 &
