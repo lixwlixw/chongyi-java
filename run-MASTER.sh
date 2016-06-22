8e538dfa813b7164adfafe8ca0ec0b530c15559f
MySQL <871810908@qq.com>
2016-06-22 13:19:19 +0800

    Create run-MASTER.sh

---
+++ run-MASTER.sh
@@ -0,0 +1,30 @@
+LIB=$PATH
+for jarfile in `ls /lib/skyEye/AV-lib/*.jar`
+
+do
+ export LIB=$LIB:$jarfile
+done
+
+for jarfile in `ls /lib/skyEye/kafka_2.11-0.9.0.0/libs/*.jar`
+do
+ export LIB=$LIB:$jarfile
+done
+
+for jarfile in `ls /usr/lib/hbase/lib/*.jar`
+do
+ export LIB=$LIB:$jarfile
+done
+
+for jarfile in `ls /usr/lib/hadoop/hadoop-*.jar`
+do
+ export LIB=$LIB:$jarfile
+done
+
+for jarfile in `ls /usr/lib/hadoop/lib/*.jar`
+do
+ export LIB=$LIB:$jarfile
+done
+
+#echo CLASSPATH=$CLASSPATH
+
+java -cp  $LIB:AV-Master.jar com.main.AV_Master >>1.log 2>&1 &
